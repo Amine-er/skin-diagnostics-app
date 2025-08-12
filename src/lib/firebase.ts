98867807d223234f126f00e160f1952702d44c57
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-08-13 00:57:45 +0100

    feat: update configuration for project setup

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'skindeep-insights-ktspu',
-  appId: '1:607151478583:web:ec44dd3d41d8a47e996cb2',
-  storageBucket: 'skindeep-insights-ktspu.firebasestorage.app',
-  apiKey: 'AIzaSyBHv8q_3OVmj3w0LvzRiYKg3yrIa1wLBUs',
-  authDomain: 'skindeep-insights-ktspu.firebaseapp.com',
+  projectId: 'test-project-ktspu',
+  appId: '1:1234567890:web:1234567890abcdef123456',
+  storageBucket: 'test-project-ktspu.appspot.com',
+  apiKey: 'AIzaSyD-1234567890abcdef1234567890abcdef',
+  authDomain: 'test-project-ktspu.firebaseapp.com',
   measurementId: '',
-  messagingSenderId: '607151478583',
+  messagingSenderId: '1234567890',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
