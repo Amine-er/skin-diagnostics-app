c8127168bdef83947d662e1307c9e9ff04b0c775
Errabi Mohamed Amine <errabi.ami@gmail.com>
2025-08-13 01:28:38 +0100

    feat: update Firebase configuration to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'test-project-ktspu',
-  appId: '1:1234567890:web:1234567890abcdef123456',
-  storageBucket: 'test-project-ktspu.appspot.com',
-  apiKey: 'AIzaSyD-1234567890abcdef1234567890abcdef',
-  authDomain: 'test-project-ktspu.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '1234567890',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
